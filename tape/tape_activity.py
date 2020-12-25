f149f7a83d304bfcec42038fb13f06c255e98aee
manhdoi <manhtran40kc@gmail.com>
2020-12-25 18:03:58 +0700

    context and activity

--- tape/tape_activity.py
+++ tape/tape_activity.py
@@ -4,3 +4,7 @@ from core.activity import Activity
 class TapeActivity(Activity):
     def execute(self, context):
         super(TapeActivity, self).execute(context)
+        idx = context.idx()
+        current = context.current()
+        context.set_current_value(current)
+        context.set_idx(idx + 1)
